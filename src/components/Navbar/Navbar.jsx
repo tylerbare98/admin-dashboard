a40c4e0f017e62590cbb1c76c934d93accdeccca
Tyler Bare <tylerbare98@gmail.com>
2023-03-07 19:03:30 -0600

    basic nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,46 @@
-import Typography from '@mui/material/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { InputAdornment, TextField, IconButton, Typography } from '@material-ui/core';
+import Language from '@mui/icons-material/Search';
+import Search from '@mui/icons-material/Search';
 
-export default function Navbar(){
-    return (
-      <Typography variant="h6" noWrap component="div">
-        Responsive drawer demo
-      </Typography> 
-    );
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    width: '100%',
+    maxWidth: 400,
+    border: `1px solid ${theme.palette.grey[400]}`,
+    borderRadius: theme.shape.borderRadius,
+    padding: theme.spacing(0.5, 1),
+  },
+  searchInput: {
+    flexGrow: 1,
+    marginRight: theme.spacing(1),
+  },
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <TextField
+        className={classes.searchInput}
+        variant="outlined"
+        placeholder="Search"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <Search />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <IconButton>
+        <Language />
+      </IconButton>
+      <Typography variant="body1">English</Typography>
+    </div>
+  );
 }
