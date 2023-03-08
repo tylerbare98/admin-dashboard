0e65f340efba8a5321ffa7aae5bfd95534d658c3
Tyler Bare <tylerbare98@gmail.com>
2023-03-07 20:26:56 -0600

    layout finished

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,26 +1,45 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import styles from './Sidebar.module.css'
-import SearchBar from '../SearchBar/SearchBar';
-import { Switch, FormControlLabel } from '@material-ui/core';
-import {Dashboard, Person, Store, CreditCard, LocalShipping, Psychology, AccountCircle, Logout,
-        BarChart, Notifications, SettingsSystemDaydream, Settings, Language, NotificationsNoneOutlined, 
-        LanguageOutlined, DarkModeOutlined, FullscreenExitOutlined, ChatBubbleOutline,
-        ListOutlined
-        } from '@mui/icons-material';
+import * as React from "react";
+import Badge from '@mui/material/Badge';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import styles from "./Sidebar.module.css";
+import SearchBar from "../SearchBar/SearchBar";
+import { Switch, FormControlLabel } from "@material-ui/core";
+import ListSubheader from "@mui/material/ListSubheader";
+import profilepic from '../../images/ProfilePic.jpg'
+import {
+  Dashboard,
+  Person,
+  Store,
+  CreditCard,
+  LocalShipping,
+  Psychology,
+  AccountCircle,
+  Logout,
+  BarChart,
+  Notifications,
+  SettingsSystemDaydream,
+  Settings,
+  Language,
+  NotificationsNoneOutlined,
+  LanguageOutlined,
+  DarkModeOutlined,
+  FullscreenExitOutlined,
+  ChatBubbleOutline,
+  ListOutlined,
+} from "@mui/icons-material";
 
 const drawerWidth = 240;
 
@@ -34,16 +53,77 @@ export default function Sidebar(props) {
 
   const drawer = (
     <>
-      <Toolbar className={styles.top}><Typography color="primary" variant="h5">Administration</Typography></Toolbar>
-        
+      <Toolbar className={styles.top}>
+        <Typography color="primary" variant="h5">
+          Administration
+        </Typography>
+      </Toolbar>
+
       <Divider />
-      <List>
-        {[['Dashboard', <Dashboard />]].map((item) => (
+      <List subheader={<ListSubheader>MAIN</ListSubheader>}>
+        {[["Dashboard", <Dashboard />]].map((item) => (
           <ListItem key={item[0]} disablePadding>
             <ListItemButton>
-              <ListItemIcon>
-                {item[1]}
-              </ListItemIcon>
+              <ListItemIcon>{item[1]}</ListItemIcon>
+              <ListItemText primary={item[0]} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List subheader={<ListSubheader>LISTS</ListSubheader>}>
+        {[
+          ["Users", <Person />],
+          ["Products", <Store />],
+          ["Orders", <CreditCard />],
+          ["Delivery", <LocalShipping />],
+        ].map((item) => (
+          <ListItem key={item[0]} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>{item[1]}</ListItemIcon>
+              <ListItemText primary={item[0]} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List subheader={<ListSubheader>USEFUL</ListSubheader>}>
+        {[
+          ["Stats", <BarChart />],
+          ["Notifications", <Notifications />],
+        ].map((item) => (
+          <ListItem key={item[0]} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>{item[1]}</ListItemIcon>
+              <ListItemText primary={item[0]} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List subheader={<ListSubheader>SERVICE</ListSubheader>}>
+        {[
+          ["System Health", <SettingsSystemDaydream />],
+          ["Logs", <Psychology />],
+          ["Settings", <Settings />],
+        ].map((item) => (
+          <ListItem key={item[0]} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>{item[1]}</ListItemIcon>
+              <ListItemText primary={item[0]} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List subheader={<ListSubheader>USER</ListSubheader>}>
+        {[
+          ["Profile", <AccountCircle />],
+          ["Logout", <Logout />],
+        ].map((item) => (
+          <ListItem key={item[0]} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>{item[1]}</ListItemIcon>
               <ListItemText primary={item[0]} />
             </ListItemButton>
           </ListItem>
@@ -51,79 +131,21 @@ export default function Sidebar(props) {
       </List>
       <Divider />
       <List>
-        {[['Users', <Person/>], ['Products',<Store/>], ['Orders',<CreditCard/>],
-         ['Delivery',<LocalShipping/>]].map((item) => (
-          <ListItem key={item[0]} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-              {item[1]}
-              </ListItemIcon>
-              <ListItemText primary={item[0]} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {[['Stats',<BarChart/>], ['Notifications', <Notifications/>]].map((item) => (
-          <ListItem key={item[0]} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-              {item[1]}
-              </ListItemIcon>
-              <ListItemText primary={item[0]} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {[['System Health',<SettingsSystemDaydream/>], ['Logs',<Psychology/>], ['Settings', <Settings/>]].map((item) => (
-          <ListItem key={item[0]} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-              {item[1]}
-              </ListItemIcon>
-              <ListItemText primary={item[0]} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {[['Profile',<AccountCircle/>], ['Logout',<Logout/>]].map((item) => (
-          <ListItem key={item[0]} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-              {item[1]}
-              </ListItemIcon>
-              <ListItemText primary={item[0]} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-            <div className={styles.bottom}>
-              <FormControlLabel
-                  control={
-                      <Switch
-                      checked={true}
-                      name="darkMode"
-                      color="primary"
-                      />
-                  }
-                  label={'Dark'}
-                />
-            </div>
+        <div className={styles.bottom}>
+          <FormControlLabel
+            control={<Switch checked={true} name="darkMode" color="primary" />}
+            label={"Dark"}
+          />
+        </div>
       </List>
     </>
   );
 
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar
         position="fixed"
@@ -138,11 +160,11 @@ export default function Sidebar(props) {
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
             <MenuIcon />
           </IconButton>
-          <SearchBar/>
+          <SearchBar />
           {/* below is the right side stuff of navbar */}
           <div className={styles.rightSideNavBar}>
             <IconButton>
@@ -154,15 +176,20 @@ export default function Sidebar(props) {
             <IconButton>
               <FullscreenExitOutlined />
             </IconButton>
-            <IconButton>
-              <NotificationsNoneOutlined />
-            </IconButton>
+            <Badge badgeContent={4} color='error' overlap="circular">
+              <IconButton>
+                <NotificationsNoneOutlined />
+              </IconButton>
+            </Badge>
+            <Badge badgeContent={2} color='error' overlap="circular">
             <IconButton>
               <ChatBubbleOutline />
             </IconButton>
+            </Badge>
             <IconButton>
               <ListOutlined />
             </IconButton>
+            <img src={profilepic} alt="Profile Image" className={styles.profilepic}></img>
           </div>
           {/* above is the right side stuff of navbar */}
         </Toolbar>
@@ -182,8 +209,11 @@ export default function Sidebar(props) {
             keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
         >
           {drawer}
@@ -191,8 +221,11 @@ export default function Sidebar(props) {
         <Drawer
           variant="permanent"
           sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
           open
         >
@@ -201,33 +234,39 @@ export default function Sidebar(props) {
       </Box>
       <Box
         component="main"
-        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
       >
         <Toolbar />
         <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+          dolor purus non enim praesent elementum facilisis leo vel. Risus at
+          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
+          quisque non tellus. Convallis convallis tellus id interdum velit
+          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
+          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
+          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+          faucibus et molestie ac.
         </Typography>
         <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
+          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
+          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
+          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
+          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
+          morbi tristique senectus et. Adipiscing elit duis tristique
+          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
           eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
           posuere sollicitudin aliquam ultrices sagittis orci a.
         </Typography>
